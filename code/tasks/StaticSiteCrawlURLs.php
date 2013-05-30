2d8b4e038808b82a77b8222de3b6e6861bef9402
Stig Lindqvist <stig@stojg.net>
2013-05-30 12:34:28 +1200

    NEW Adding StaticSiteCrawlURLs task
    This allows the url crawling to be run from a cli environment

---
+++ code/tasks/StaticSiteCrawlURLs.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * StaticSiteCrawlURLs
+ *
+ */
+class StaticSiteCrawlURLs extends BuildTask {
+
+	function run($request) {
+		$id = $request->getVar('ID');
+		if(!is_numeric($id) || !$id) {
+			echo "<p>Specify ?ID=(number)</p>";
+			return;
+		}
+		// Find all pages
+		$contentSource = StaticSiteContentSource::get()->byID($id);
+		$contentSource->urllist()->crawl(false, true);
+	}
+
+}
