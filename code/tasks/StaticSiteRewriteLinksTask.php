0620fb9d9eb89de52deb10660c993d643c2c1fad
Russell Michell <russell@silverstripe.com>
2013-06-24 16:25:20 +1200

    - Cleaned-up the warnings and notices functionality into own method - Added logging for failed URL rewrites to analyse what's failing - Added some URL cleanup routines to the URL Rewrite task which improve the hit-rate as per data from the log

--- code/tasks/StaticSiteRewriteLinksTask.php
+++ code/tasks/StaticSiteRewriteLinksTask.php
@@ -2,39 +2,60 @@
 
 /**
  * Rewrite all links in content imported via staticsiteimporter
+ *
+ * @todo deal-to Files and Images also
  */
 class StaticSiteRewriteLinksTask extends BuildTask {
+
+	/**
+	 * Where the failure log is cached
+	 */
+	public static $failure_log = 'failedRewrite.log';
+
+	/**
+	 * Stores the dodgy URLs for later analysis
+	 *
+	 * @var type array
+	 */
+	public $listFailedRewrites = array();
 	
 	function run($request) {
 		$id = $request->getVar('ID');
 		if(!is_numeric($id) || !$id) {
-			echo "<p>Specify ?ID=(number)</p>";
+			$this->printMessage("Specify ?ID=(number)",'WARNING');
 			return;
 		}
 
 		// Find all pages
 		$contentSource = StaticSiteContentSource::get()->byID($id);
 		$pages = $contentSource->Pages();
+		$files = $contentSource->Files();
 
-		echo "<p>Looking through " . $pages->Count() . " pages</p>\n";
+		$this->printMessage("Looking through {$pages->count()} pages.",'NOTICE');
 
 		// Set up rewriter
 		$pageLookup = $pages->map('StaticSiteURL', 'ID');
+		$fileLookup = $files->map('StaticSiteURL', 'ID');
 		$baseURL = $contentSource->BaseUrl;
+		$task = $this;
 
-		$rewriter = new StaticSiteLinkRewriter(function($url) use($pageLookup, $baseURL) {
+		$rewriter = new StaticSiteLinkRewriter(function($url) use($pageLookup, $fileLookup, $baseURL, $task) {
 			$fragment = "";
 			if(strpos($url,'#') !== false) {
 				list($url,$fragment) = explode('#', $url, 2);
 				$fragment = '#'.$fragment;
 			}
 
+			$url = $task->urlCleanup($url);
 			if($pageLookup[$url]) {
 				return '[sitetree_link,id='.$pageLookup[$url] .']' . $fragment;
-			
-			} else {
+			}
+			else if($fileLookup[$url]) {
+				return '[file_link,id='.$fileLookup[$url] .']';
+			}
+			else {
 				if(substr($url,0,strlen($baseURL)) == $baseURL) {
-					echo "<p>WARNING: $url couldn't be rewritten.</p>\n";
+					$task->printMessage("{$url} couldn't be rewritten",'WARNING',$url);
 				}
 				return $url . $fragment;
 			}
@@ -45,13 +66,18 @@ class StaticSiteRewriteLinksTask extends BuildTask {
 		foreach($pages as $page) {
 
 			$schema = $contentSource->getSchemaForURL($page->URLSegment);
+			if(!$schema) {
+				$this->printMessage("No schema found for {$page->URLSegment}",'WARNING');
+				continue;
+			}
 			// Get fields to process
 			$fields = array();
 			foreach($schema->ImportRules() as $rule) {
-				if(!$rule->PlainText) $fields[] = $rule->FieldName;
+				if(!$rule->PlainText) {
+					$fields[] = $rule->FieldName;
+				}
 			}
 			$fields = array_unique($fields);
-			
 
 			foreach($fields as $field) {
 				$newContent = $rewriter->rewriteInContent($page->$field);
@@ -59,14 +85,57 @@ class StaticSiteRewriteLinksTask extends BuildTask {
 					$newContent = str_replace(array('%5B','%5D'),array('[',']'),$newContent);
 					$changedFields++;
 
-					echo "<p>Changed $field on $page->Title (#$page->ID).</p>";
+					$this->printMessage("Changed {$field} on \"{$page->Title}\" (#{$page->ID})",'NOTICE');
 					$page->$field = $newContent;
 				}
 			}
 
 			$page->write();
 		}
-		echo "<p>DONE. Amended $changedFields content fields.</p>".PHP_EOL;
+		$this->printMessage("Amended {$changedFields} content fields.",'SYSTEM');
+		$this->writeFailedRewrites();
+	}
 
-	} 
+	/*
+	 * Prints notices and warnings and aggregates them into two lists for later analysis, depending on $level and whether you're using the CLI or a browser
+	 * 
+	 * @param string $msg
+	 * @param string $level
+	 * @param string $baseURL
+	 */
+	public function printMessage($msg,$level,$url=null) {
+		if(Director::is_cli()) {
+			echo "[{$level}] {$msg}".PHP_EOL;
+		}
+		else {
+			echo "<p>[{$level}] {$msg}</p>".PHP_EOL;
+		}
+		if($url && $level == 'WARNING') {
+			array_push($this->listFailedRewrites,"Couldn't rewrite: {$url}");
+		}
+	}
+
+	/*
+	 * Write failed rewrites to a logfile for later analysis
+	 */
+	public function writeFailedRewrites() {
+		$logFile = getTempFolder().'/'.self::$failure_log;
+		$logFail = implode(PHP_EOL,$this->listFailedRewrites);
+		$totalFailures = 'Total Failures: '.sizeof($this->listFailedRewrites).PHP_EOL;
+		$logData = $totalFailures.PHP_EOL.$logFail.PHP_EOL.'----'.PHP_EOL;
+		file_put_contents($logFile, $logData, FILE_APPEND);
+	}
+
+	/*
+	 * post-process URLs so DB matches in `SiteTee.StaticSiteURL` are more easily found
+	 *
+	 * @param string $url
+	 * @return string $url
+	 * @todo Is this logic better located in `SiteTree.StaticSiteURLList`?
+	 */
+	public function urlCleanup($url) {
+		// Clean-up urlencoded spaces, trailing slashes etc. This increaes our hit rate
+		$url = preg_replace("#^(.+)/$#","$1",str_replace('%20',' ',$url));
+		return $url;
+	}
 }
