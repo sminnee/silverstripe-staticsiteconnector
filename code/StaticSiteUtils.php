85f4680a05771aa6bb599404cbd9319bdf8a2613
Russell Michell <russell@silverstripe.com>
2014-03-24 22:08:13 +1300

    MINOR: Removed now redundant method.

--- code/StaticSiteUtils.php
+++ code/StaticSiteUtils.php
@@ -30,42 +30,6 @@ class StaticSiteUtils {
 	}
 
 	/**
-	 * Should be called prior to import. It resets all items' StaticSiteURL fields to NULL. 
-	 * If there are multiple imported sub-trees, this will clear the first-import's StaticSiteURL 
-	 * so the rewrite task only rewrites links to content with a StaticSiteURL value that is NOT NULL.
-	 * 
-	 * If this isn't done, it isn't clear to the RewriteLinks task, which tree of imported content 
-	 * to look in for content to link-to when multiple imports from the same legacy site have been made. 
-	 * In this case it will clumsily look for _all_ imported content with a matching SiteTree.StaticSiteURL.
-	 *
-	 * @param string $absoluteURL The Absolute URL as it looked when scraped from the legacy site.
-	 * @param number $sourceID The ID of the relevant StaticSiteContentSource
-	 * @param string $class 'SiteTree', 'File' or 'Image'
-	 * @todo Figure out how to ensure these updates happen for the correct item, not _all_ items..
-	 * @todo this is only useful when there are multiple subtrees. Detect if there are, and only run if there are >=2.
-	 * @todo The match may be failing becuase StaticSiteURLs contain http(s)?://(www\.)?.
-	 * @return void
-	 */
-	public function resetStaticSiteURLs($absoluteURL, $sourceID, $class) {
-		$resetItems = DataObject::get($class)->filter(array(
-			'StaticSiteContentSourceID' => $sourceID,
-			'StaticSiteURL' => $absoluteURL
-		));
-		
-		foreach($resetItems as $item) {
-			$item->StaticSiteURL = NULL;
-			if($item->ClassName == 'SiteTree') {
-				Versioned::reading_stage('Stage');
-				$item->write();
-				$item->publish('Stage', 'Live');
-			}
-			else {
-				$item->write();
-			}	
-		}
-	}
-
-	/**
 	 * If operating in a specific environment, set some proxy options for it for passing to curl and 
 	 * to phpCrawler (if set in config).
 	 *
