c8bed85a2c6b99e19e3a7088a87c29d2d5a781b3
Russell Michell <russell@silverstripe.com>
2014-04-14 21:22:35 +1200

    Added clear imports logic: - FailedURLRewriteObject - FailedURLRewriteSummary - StaticSiteImportDataObject

--- code/models/FailedURLRewriteSummary.php
+++ code/models/FailedURLRewriteSummary.php
@@ -3,8 +3,8 @@
  * A model object that represents a single failed link-rewrite summary. This data is displayed
  * at the top of the {@link FailedURLRewriteReport}.
  * 
+ * @author Russell Michell <russ@silverstripe.com>
  * @package staticsiteconnector
- * @author Russell Michell <russell@silverstripe.com>
  */
 class FailedURLRewriteSummary extends DataObject {
 
